3582d05a165350c84311936c486f33a3dc8100ec
Михаил Агальцов <it145@maria-ra.ru>
2018-11-30 00:22:31 +0700

    Удалил комментарий с примером запуска выгрузки из bat.

--- precommit.py
+++ precommit.py
@@ -4,8 +4,6 @@ from icdump import base_dump, GEAR, BASE_DEVELOPER
 
 # initialize master dump
 
-# %GEAR% %BASE_DEV% /ConfigurationRepositoryUpdateCfg -force /DumpConfigToFiles C:\dump -update 
-
 subprocess.call('%s %s /ConfigurationRepositoryUpdateCfg -force /DumpConfigToFiles %s -update -configDumpInfoForChanges %s\\ConfigDumpInfo.xml'%(GEAR, BASE_DEVELOPER, base_dump.DEVELOPER, base_dump.MASTER))
 
 
