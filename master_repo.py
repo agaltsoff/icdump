6a328d23e41d28411d62b898e8cd970af3378902
Михаил Агальцов <agaltsoff@gmail.com>
2019-03-21 13:35:40 +0700

    Очщаем каталог выгрузки разработки перед выгрузкой

--- master_repo.py
+++ master_repo.py
@@ -35,15 +35,16 @@ for root, dirs, files in os.walk(base_dump.REPO):
 
 for root, dirs, files in os.walk(base_dump.DEVELOPER):
     for dev_f in files:
+        if dev_f == 'ConfigDumpInfo.xml': continue
         dev_fn= os.path.join(root, dev_f)
         dev_fnrp= os.path.relpath(dev_fn, base_dump.DEVELOPER)
         if not dev_fnrp in repo_files:
             src= os.path.join(base_dump.MASTER, dev_fnrp)
             dst= os.path.join(base_dump.REPO, dev_fnrp)
             path= os.path.dirname(dst)
-##            if os.path.exists(src):
-##                if not os.path.exists(path):
-##                    os.makedirs(path)
+            if os.path.exists(src):
+                if not os.path.exists(path):
+                    os.makedirs(path)
 ##                shutil.copy(src, dst)
                 print(src, ' -> ', dst)
 
--- precommit.py
+++ precommit.py
@@ -1,8 +1,15 @@
+import os
+import shutil
 import subprocess
 
 from icdump import base_dump, GEAR, BASE_DEVELOPER
 
 # initialize master dump
+
+#clear dev dir first
+
+if os.path.exists(base_dump.DEVELOPER): shutil.rmtree(base_dump.DEVELOPER)
+os.mkdir(base_dump.DEVELOPER)
 
 subprocess.call('%s %s /ConfigurationRepositoryUpdateCfg -force'%(GEAR, BASE_DEVELOPER))
 subprocess.call('%s %s /DumpConfigToFiles %s -update -configDumpInfoForChanges %s\\ConfigDumpInfo.xml'%(GEAR, BASE_DEVELOPER, base_dump.DEVELOPER, base_dump.MASTER))
