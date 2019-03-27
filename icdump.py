dfbc52c7a89099844810657dea9a7eff43479fa6
Михаил Агальцов <agaltsoff@gmail.com>
2019-03-27 16:39:52 +0700

    precommit

--- icdump.py
+++ icdump.py
@@ -1,6 +1,8 @@
+import os
+import shutil
 import subprocess
 
-from init import base_dump, GEAR, BASE_MASTER
+from init import base_dump, GEAR, BASE_MASTER, BASE_DEVELOP
 
 # initialize master dump
 def setup():
@@ -12,4 +14,16 @@ def update():
     subprocess.call('%s %s /ConfigurationRepositoryUpdateCfg -force'%(GEAR, BASE_MASTER))
     subprocess.call('%s %s /DumpConfigToFiles %s -update'%(GEAR, BASE_MASTER, base_dump.MASTER))
 
+# unload develop diffs from master
+def precommit():
+
+    # clear dev dir first
+
+    if os.path.exists(base_dump.DEVELOP): 
+        shutil.rmtree(base_dump.DEVELOP)
+
+    os.mkdir(base_dump.DEVELOP)
+
+    subprocess.call('%s %s /ConfigurationRepositoryUpdateCfg -force'%(GEAR, BASE_DEVELOP))
+    subprocess.call('%s %s /DumpConfigToFiles %s -update -configDumpInfoForChanges %s\\ConfigDumpInfo.xml'%(GEAR, BASE_DEVELOP, base_dump.DEVELOP, base_dump.MASTER))
 
--- precommit.py
+++ precommit.py
@@ -1,21 +1,6 @@
-import os
-import shutil
-import subprocess
+from icdump import precommit
 
-from icdump import base_dump, GEAR, BASE_DEVELOP
-
-# initialize master dump
-
-# clear dev dir first
-
-if os.path.exists(base_dump.DEVELOP): 
-    shutil.rmtree(base_dump.DEVELOP)
-
-os.mkdir(base_dump.DEVELOP)
-
-subprocess.call('%s %s /ConfigurationRepositoryUpdateCfg -force'%(GEAR, BASE_DEVELOP))
-subprocess.call('%s %s /DumpConfigToFiles %s -update -configDumpInfoForChanges %s\\ConfigDumpInfo.xml'%(GEAR, BASE_DEVELOP, base_dump.DEVELOP, base_dump.MASTER))
-
+precommit()
 
 
 
