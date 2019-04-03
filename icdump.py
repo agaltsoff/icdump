03f4ddaadb0a642d79aad2a82951ce6e85e4a15e
Михаил Агальцов <agaltsoff@gmail.com>
2019-04-03 09:08:22 +0700

    rollback загрузка конфигурации из файлов, не работает. debug в параметрах командной строки

--- icdump.py
+++ icdump.py
@@ -2,6 +2,8 @@ import os
 import shutil
 import subprocess
 
+from codecs import open
+
 from icdumplib import args, base_dump, is_skipped, GEAR, BASE_MASTER, BASE_DEVELOP
 
 # initialize master dump
@@ -17,17 +19,16 @@ def update():
 # unload develop diffs from master
 def precommit():
 
-    # clear dev dir first
+    # clear develop dir first
 
-    if os.path.exists(base_dump.DEVELOP): 
-        shutil.rmtree(base_dump.DEVELOP)
+    if os.path.exists(base_dump.DEVELOP): shutil.rmtree(base_dump.DEVELOP)
 
     os.mkdir(base_dump.DEVELOP)
 
     subprocess.call('%s %s /ConfigurationRepositoryUpdateCfg -force'%(GEAR, BASE_DEVELOP))
     subprocess.call('%s %s /DumpConfigToFiles %s -update -configDumpInfoForChanges %s\\ConfigDumpInfo.xml'%(GEAR, BASE_DEVELOP, base_dump.DEVELOP, base_dump.MASTER))
 
-# delete repo files that are not in developer files from repo
+# delete repo files that are not in develop files from repo
 def master():
 
     dev_files= [] # contains relative paths
@@ -48,7 +49,7 @@ def master():
                 repo_fn= os.path.join(base_dump.REPO, repo_fnrp)
                 os.remove(repo_fn)
 
-    ## copy developer files that are not in repo from master to repo
+    # copy develop files that are not in repo from master to repo
 
     repo_files= []
 
@@ -73,7 +74,7 @@ def master():
                         os.makedirs(path)
                     shutil.copy(src, dst)
 
-# copy files from dump to repo
+# copy develop files to repo
 def develop():
     for root, dirs, files in os.walk(base_dump.DEVELOP):
         for dev_f in files:
@@ -86,10 +87,11 @@ def develop():
             if not os.path.exists(path): os.makedirs(path)
             shutil.copy(src, dst)
 
+#upload repo files to develop configuration
 def checkout():
-    print('Not implemented')
+    subprocess.call('%s %s /LoadConfigFromFiles %s -AllWritable'%(GEAR, BASE_DEVELOP, base_dump.REPO))
 
-# create rollback directory of master files that are in develop and upload them to develop configuration
+# create rollback directory of master files that are developed and upload them to develop configuration
 def rollback():
 
     rollback_path= os.path.join(args.path, 'rollback')
@@ -103,9 +105,27 @@ def rollback():
             develop_rp= os.path.relpath(os.path.join(root, develop_fn), base_dump.DEVELOP)
             master_fn= os.path.join(base_dump.MASTER, develop_rp)
             rollback_fn= os.path.join(rollback_path, develop_rp)
-            rollback_path= os.path.dirname(rollback_fn)
-            if not os.path.exists(rollback_path): os.makedirs(rollback_path)
+            rollback_fn_path= os.path.dirname(rollback_fn)
+            if not os.path.exists(rollback_fn_path): os.makedirs(rollback_fn_path)
             shutil.copy(master_fn, rollback_fn)
+
+    # creating rollback file list for uploading
+
+    file_list= ''
+
+    for root, dirs, files in os.walk(rollback_path):
+        if root != rollback_path:
+            for fn in files: file_list+= os.path.join(root, fn) + '\n'
+
+    if file_list != '': file_list= file_list[:-1]
+
+    list_fn= os.path.join(rollback_path, 'filelist.txt');
+
+    with open(list_fn, 'w', 'utf-8') as list_file:
+        list_file.write(file_list)
+
+    subprocess.call('%s %s /LoadConfigFromFiles "%s" -ListFiles "%s" -AllWritable'%(GEAR, BASE_DEVELOP, rollback_path, list_fn))
+            
 
 def commit():
     print('Not implemented')
--- icdumplib.py
+++ icdumplib.py
@@ -26,24 +26,25 @@ def debug_remove(fn):
 def debug_makedirs(fn):
     print('++', fn)
 
-def debug_copy(scr, dst):
+def debug_copy(src, dst):
     print(src, '->', dst)
 
 commands= {
     'setup'     : 'Начальная выгрузка эталона',
-    'commit'    : 'git commit (не реализовано)',
     'update'    : 'Выгрузка изменений эталона',
     'precommit' : 'Выгрузка отличий разработки от эталона',
     'master'    : 'Перенос отличий из эталона в репозиторий',
     'develop'   : 'Перенос отличий из разработки в репозиторий',
-    'checkout'  : 'Перенос изменений текущей ветки в рарзрабоку',
-    'rollback'  : 'Восстановление начального состояния разработки',
+    'checkout'  : 'Загрузка файлов репозитория в конфигурацию разработки',
+    'rollback'  : 'Отмена изменений конфигурации разработки',
+    'commit'    : 'git commit (не реализовано)',
 }
 
 parser = argparse.ArgumentParser(description='Выгрузка начального состояния изменений конфигурации 1С типовыми средствами', formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('command', metavar='COMMAND', type=str, nargs= '+', choices= commands.keys(), help='\n'.join("%s\t%s" % (name, descr) for (name, descr) in commands.items()))
 parser.add_argument('--config', metavar='CONFIG', type=str, help='Файл настроек в формате json', default='.\\config.json')
 parser.add_argument('--path', metavar='PATH', type=str, help='Рабочий каталог', default='.\\')
+parser.add_argument('--debug', help='Режим отладки', action="store_true")
 
 args= parser.parse_args()
 
@@ -63,9 +64,7 @@ BASE_MASTER= get_base_connect_str(base_master)
 
 BASE_DEVELOP= get_base_connect_str(base_develop)
 
-debug= init["debug"] if "debug" in init else False
-
-if debug:
+if args.debug:
     shutil.copy= debug_copy
     os.remove= debug_remove
     os.makedirs= debug_makedirs
